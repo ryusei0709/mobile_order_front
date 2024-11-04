3680fa19ff35606270a745339c4f0c662de5ddaa
tsuchiya <tsuchiya@ook-Pro.local>
2024-11-04 14:56:42 +0900

    muiの設定

---
+++ src/app/(pages)/(menu)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MenuRootPage = () => {
+  return (
+    <div>
+        menu
+    </div>
+  )
+}
+
+export default MenuRootPage
--- src/app/(pages)/(test1)/request/page.tsx
+++ src/app/(pages)/(test1)/request/page.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react'
 
 const Test1Page = () => {
