d2a3f3b79324013a51bb46fb6fcec94377e23167
tsuchiya <tsuchiya@ook-Pro.local>
2024-11-04 12:34:48 +0900

    storybook/yarn/mui init

---
+++ src/app/(pages)/(test1)/request/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test1Page = () => {
+  return (
+    <div>
+        test page
+    </div>
+  )
+}
+
+export default Test1Page
---
+++ src/app/(pages)/(test2)/fetch/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      page2
+    </div>
+  )
+}
+
+export default page
