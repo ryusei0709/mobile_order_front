051ba55c45c29aec1a75e82ffc655cd69b93566c
tsuchiya <tsuchiya@ook-Pro.local>
2024-11-10 18:32:35 +0900

    redux store test

---
+++ src/app/(pages)/(menu)/menu/[menuId]/page.tsx
@@ -0,0 +1,15 @@
+import Description from '@/app/features/menuDetail/Description'
+import MenuDetailContainer from '@/app/features/menuDetail/MenuDetailContainer'
+import Opstion from '@/app/features/menuDetail/Opstion'
+import React from 'react'
+
+const MeunDetailPage = () => {
+  return (
+    <>
+      <MenuDetailContainer />
+    </>
+  )
+}
+
+export default MeunDetailPage
+
